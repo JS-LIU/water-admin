8294ca79b52aa8cab7dbf23923ff79309e734c94
JS-LIU <984003917@qq.com>
2018-07-19 19:15:51 +0800

    提现

--- src/store/finance/WithdrawItem.js
+++ src/store/finance/WithdrawItem.js
@@ -1,11 +1,21 @@
 /**
  * Created by LDQ on 2018/6/14
  */
-
+import _h from "../../Util/HB";
 class WithdrawItem{
     constructor(withdrawInfo){
         this.withdrawId = withdrawInfo.id;
+        this.accountId = withdrawInfo.withdrawInfo;
+        let withdrawAjax = _h.ajax.resource("/admin/withdraw/:action");
+        this._allowWithdraw = function(postInfo){
+            return withdrawAjax.save({action:"allow"},postInfo);
+        }
 
     }
+    allow(remarks){
+        return this._allowWithdraw({orderId:this.withdrawId,remarks:remarks});
+    }
+
+
 }
 module.exports = WithdrawItem;
--- src/store/finance/WithdrawList.js
+++ src/store/finance/WithdrawList.js
@@ -3,12 +3,29 @@
  */
 import AdminList from '../AdminList';
 import _h from "../../Util/HB";
+import WithdrawItem from "./WithdrawItem";
 
 class WithdrawList extends AdminList{
     constructor() {
         super();
-        // this.;
-        let withdrawAjax = _h.ajax.resource();
+        let withdrawAjax = _h.ajax.resource("/admin/withdraw/:action");
+        this.orderStatus = ['create',"rejected","finish"];              //  create,rejected,finish
+        this._withdrawList = function(postInfo){
+            return withdrawAjax.save({action:"list"},postInfo);
+        };
+        this.status = {
+            "all":()=>{this.orderStatus = ['create',"rejected","finish"]},
+            "waitWithdraw":()=>{this.orderStatus = ['create']},
+            "finish":()=>{this.orderStatus = ['finish']}
+        }
+    }
+    setStatus(status){
+        return this.status[status];
+    }
+    getWithdrawList(){
+        let queryMsg = Object.assign({},this.queryMsg,{orderStatus: this.orderStatus});
+        let reqMsg = {queryInfoMsg:queryMsg};
+        return this.getList(reqMsg,this._withdrawList,WithdrawItem);
     }
 
 }
