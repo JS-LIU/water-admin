5c6af0f6843e51e5ce78684d69e52f127882287e
xiannvrong <1796940658@qq.com>
2018-07-26 14:49:58 +0800

    目录icon更改

--- src/container/HomeMenuView.js
+++ src/container/HomeMenuView.js
@@ -18,36 +18,36 @@ class HomeMenuView extends Component{
                 <Menu theme="dark"
                       mode="inline"
                       defaultSelectedKeys={[defaultSelectedKey]}>
-                    <SubMenu key="sub1" title={<span><Icon type="profile" /><span>用户订单</span></span>}>
+                    <SubMenu key="sub1" title={<span><Icon type="user" /><span>用户订单</span></span>}>
                         <Menu.Item key="/clientOrder">
                             <Link to="/clientOrder">
-                                <Icon type="file" />
+                                <Icon type="edit" />
                                 <span>订单处理</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/clientOrderSearch">
                             <Link to="/clientOrderSearch">
-                                <Icon type="file" />
+                                <Icon type="search" />
                                 <span>订单查询</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/orderForms">
                             <Link to="/clientOrder">
-                                <Icon type="file" />
+                                <Icon type="file-text" />
                                 <span>订单报表</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub2" title={<span><Icon type="profile" /><span>商家订单</span></span>}>
+                    <SubMenu key="sub2" title={<span><Icon type="team" /><span>商家订单</span></span>}>
                         <Menu.Item key="/merchantOrder">
                             <Link to="/merchantOrder">
-                                <Icon type="file" />
+                                <Icon type="edit" />
                                 <span>订单处理</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/merchantOrderSearch">
                             <Link to="/merchantOrderSearch">
-                                <Icon type="file" />
+                                <Icon type="search" />
                                 <span>订单查询</span>
                             </Link>
                         </Menu.Item>
@@ -58,154 +58,154 @@ class HomeMenuView extends Component{
                         {/*</Link>*/}
                         {/*</Menu.Item>*/}
                     </SubMenu>
-                    <SubMenu key="sub3" title={<span><Icon type="pay-circle-o" /><span>财务</span></span>}>
+                    <SubMenu key="sub3" title={<span><Icon type="bank" /><span>财务</span></span>}>
                         <Menu.Item key="/withdraw">
                             <Link to="/withdraw">
-                                <Icon type="bank" />
+                                <Icon type="pay-circle-o" />
                                 <span>提现订单</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/withdrawSearch">
                             <Link to="/withdrawSearch">
-                                <Icon type="bank" />
+                                <Icon type="pay-circle-o" />
                                 <span>提现查询</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/accountList">
                             <Link to="/accountList">
-                                <Icon type="solution" />
+                                <Icon type="wallet" />
                                 <span>账户列表</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/rebateDeal">
                             <Link to="/rebateDeal">
-                                <Icon type="solution" />
+                                <Icon type="red-envelope" />
                                 <span>返利处理</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/rebateSearch">
                             <Link to="/rebateSearch">
-                                <Icon type="solution" />
+                                <Icon type="red-envelope" />
                                 <span>返利查询</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/deal">
                             <Link to="/deal">
-                                <Icon type="file" />
+                                <Icon type="login" />
                                 <span>成交</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/expenditure">
                             <Link to="/expenditure">
-                                <Icon type="file" />
+                                <Icon type="logout" />
                                 <span>支出</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub4" title={<span><Icon type="profile" /><span>商家管理</span></span>}>
+                    <SubMenu key="sub4" title={<span><Icon type="shop" /><span>商家管理</span></span>}>
                         <Menu.Item key="/merchantAudit">
                             <Link to="/merchantAudit">
-                                <Icon type="shop" />
+                                <Icon type="usergroup-add" />
                                 <span>商家入驻</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/merchantAuditQuery">
                             <Link to="/merchantAuditQuery">
-                                <Icon type="shop" />
+                                <Icon type="search" />
                                 <span>商家查询</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/merchantDetail">
                             <Link to="/merchantDetail">
-                                <Icon type="shop" />
+                                <Icon type="solution" />
                                 <span>商家信息</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub5" title={<span><Icon type="profile"/><span>批发商品</span></span>}>
+                    <SubMenu key="sub5" title={<span><Icon type="shopping-cart" /><span>批发商品</span></span>}>
                         <Menu.Item key="/stockCategory">
                             <Link to="/stockCategory">
-                                <Icon type="shop" />
+                                <Icon type="tags-o" />
                                 <span>分类管理</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/stockProductEdit">
                             <Link to="/stockProductEdit">
-                                <Icon type="shop" />
+                                <Icon type="edit" />
                                 <span>编辑商品</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/stockProductSearch">
                             <Link to="/stockProductSearch">
-                                <Icon type="shop" />
+                                <Icon type="search" />
                                 <span>商品查询</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key='sub6' title={<span><Icon type='profile'/><span>自营商品</span></span>} >
+                    <SubMenu key='sub6' title={<span><Icon type="database" /><span>自营商品</span></span>} >
                         <Menu.Item key="/selfSaleCategory">
                             <Link to="/selfSaleCategory">
-                                <Icon type="shop" />
+                                <Icon type="appstore-o" />
                                 <span>分类管理</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/selfSaleEditProduct">
                             <Link to="/selfSaleEditProduct">
-                                <Icon type="shop" />
+                                <Icon type="edit" />
                                 <span>编辑商品</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/selfSaleProductSearch">
                             <Link to="/selfSaleProductSearch">
-                                <Icon type="shop" />
+                                <Icon type="search" />
                                 <span>商品查询</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key='sub7' title={<span><Icon type='profile'/><span>分销商品</span></span>} >
+                    <SubMenu key='sub7' title={<span><Icon type="share-alt" /><span>分销商品</span></span>} >
                         <Menu.Item key="/distributeCategory">
                             <Link to="/distributeCategory">
-                                <Icon type="shop" />
+                                <Icon type="bar-chart" />
                                 <span>分类管理</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/distributeEditProduct">
                             <Link to="/distributeEditProduct">
-                                <Icon type="shop" />
+                                <Icon type="edit" />
                                 <span>编辑商品</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/distributeProductSearch">
                             <Link to="/distributeProductSearch">
-                                <Icon type="shop" />
+                                <Icon type="search" />
                                 <span>商品查询</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key='sub8' title={<span><Icon type='profile'/><span>生产成本</span></span>} >
+                    <SubMenu key='sub8' title={<span><Icon type="pay-circle" /><span>生产成本</span></span>} >
                         <Menu.Item key="/manufactureCost">
                             <Link to="/manufactureCost">
-                                <Icon type="shop" />
+                                <Icon type="heart" />
                                 <span>喜腾山泉生产</span>
                             </Link>
                         </Menu.Item>
                     </SubMenu>
                     <Menu.Item key="openShop">
                         <Link to="/openShop">
-                            <Icon type="user" />
+                            <Icon type="laptop" />
                             <span>审核</span>
                         </Link>
                     </Menu.Item>
-                    <SubMenu key="sub9" title={<span><Icon type="pay-circle-o" /><span>测试</span></span>}>
+                    <SubMenu key="sub9" title={<span><Icon type="hdd" /><span>测试</span></span>}>
                         <Menu.Item key="/auditMerchantTest">
                             <Link to="/auditMerchantTest">
-                                <Icon type="bank" />
+                                <Icon type="laptop" />
                                 <span>店铺审核测试</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="/accountList">
                             <Link to="/accountList">
-                                <Icon type="solution" />
+                                <Icon type="file-text" />
                                 <span>账户列表</span>
                             </Link>
                         </Menu.Item>
